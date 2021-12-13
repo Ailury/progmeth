10764eebb6ba06371f62c3e8548372488de5137f
Sirapoom Leesuravanich <sirapoomlee@gmail.com>
2021-12-13 12:35:25 +0700

    collidable and collectable

---
+++ src/entity/Boss.java
@@ -0,0 +1,17 @@
+package entity;
+
+import component.Enemy;
+
+public class Boss extends Enemy {
+
+	public Boss(double x, double y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Boss(double x, double y, int r) {
+		super(x, y, r);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/entity/Monster.java
@@ -0,0 +1,17 @@
+package entity;
+
+import component.Enemy;
+
+public class Monster extends Enemy {
+
+	public Monster(double x, double y, int w, int h) {
+		super(x, y, w, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Monster(double x, double y, int r) {
+		super(x, y, r);
+		// TODO Auto-generated constructor stub
+	}
+
+}
